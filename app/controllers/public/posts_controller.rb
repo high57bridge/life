bbce7842546a986fdcab9a6739d6c73727c90d87
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2024-02-13 12:16:11 +0000

    新規登録作成中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,28 @@
 class Public::PostsController < ApplicationController
+  
+  def new_guest
+    # emailでユーザーが見つからなければ作ってくれるという便利なメソッド
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+    # 自分はユーザー登録時にニックネームを必須にしているのでこの記述が必要
+    user.nickname = "ゲスト"
+    # 英数字混合を必須にしているので、ランダムパスワードに、英字と数字を追加してバリデーションに引っかからないようにしています。
+    user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+    user.confirmed_at = Time.now
+    end
+    # sign_inはログイン状態にするデバイスのメソッド、userは3行目の変数userです。
+    sign_in user
+    # ログイン後root_pathに飛ぶようにしました。
+    redirect_to root_path
+  end
+
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  email: "webcamp@dmm.com",
+  password: "dmm202311",
+  municipality_name: "a",
+  introduction: "b"
+)
