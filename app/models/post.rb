12242b78e3d55f562190ab48088430e3d95c127e
EC2 Default User <ec2-user@ip-172-31-26-173.us-west-2.compute.internal>
2024-05-25 14:36:35 +0000

    next edit googlemaps

--- app/models/post.rb
+++ app/models/post.rb
@@ -36,6 +36,59 @@ class Post < ApplicationRecord
       post.tags << tag
     end
   end
+  
+                              <style>
+                                #map {
+                                  height: 400px;
+                                  width: 500px;
+                                }
+                            </style>
+
+                            <script>
+                                let map
+                                let geocoder
+
+                                const display = document.getElementById('display')
+
+                                function initMap(){
+                                  geocoder = new google.maps.Geocoder(),
+
+                                  map = new google.maps.Map(document.getElementById('map'), {
+                                    // latが緯度、lngが経度を示します
+                                    center: {lat: 40.7828, lng:-73.9653},
+                                    //数値は0〜21まで指定できます。数値が大きいほど拡大されます
+                                    zoom: 10,
+                                  });
+                                  //positionに指定した座標にピンを表示させます
+                                  marker = new google.maps.Marker({
+                                    position:  {lat: 40.7828, lng:-73.9653},
+                                    map: map
+                                  });
+                                }
+
+                                  //検索フォームのボタンが押された時に実行される
+                                function codeAddress(){
+                                  //検索フォームの入力内容を取得
+                                  let inputAddress = document.getElementById('address').value;
+
+                                  geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+                                    //該当する検索結果がヒットした時に、地図の中心を検索結果の緯度経度に更新する
+                                    if (status == 'OK') {
+                                      map.setCenter(results[0].geometry.location);
+                                      var marker = new google.maps.Marker({
+                                          map: map,
+                                          position: results[0].geometry.location
+                                      });
+                                      display.textContent = "検索結果：" + results[ 0 ].geometry.location
+                                    } else {
+                                      //検索結果が何もなかった場合に表示
+                                      alert('該当する結果がありませんでした：' + status);
+                                    }
+                                  });
+                                }
+                            </script>
+                            <script src="https://maps.googleapis.com/maps/api/js?key=<%= ENV["GOOGLE_API_KEY"] %>&callback=initMap" async defer></script>
+
 
 # 検索方法分岐
   def self.looks(search, word)
